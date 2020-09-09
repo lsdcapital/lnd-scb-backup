ca7c50e1aca46e66fac363a899158791f2eafb5f
Stefan Lesicnik <stefan@lsdopen.io>
2020-09-09 10:45:18 +0100

    Testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import grpc
 import os
 
 import codecs
-
+import base64
 
 with open('/mnt/hdd/lnd/data/chain/bitcoin/mainnet/admin.macaroon', 'rb') as f:
     macaroon_bytes = f.read()
@@ -17,11 +17,21 @@ creds = grpc.ssl_channel_credentials(cert)
 channel = grpc.secure_channel('localhost:10009', creds)
 stub = lnrpc.LightningStub(channel)
 
-response = stub.WalletBalance(ln.WalletBalanceRequest(), metadata=[('macaroon', macaroon)])
-print(response.total_balance)
+while True:
+    try:
+        response = stub.WalletBalance(ln.WalletBalanceRequest(), metadata=[('macaroon', macaroon)])
+        print(response.total_balance)
 
+        request = ln.ChannelBackupSubscription()
+        for response in stub.SubscribeChannelBackups(request, metadata=[('macaroon', macaroon)]):
+            print(response)
+            r = response["multi_chan_backup"]["multi_chan_backup"]
+            print(r)
+            decode = base64.b64decode(r)
+            #decode = codecs.decode(r, 'hex')
+            print(decode)
+            hex = decode.hex()
+            print(hex)
+    except:
+            print('meep')
 
-
-request = ln.ChannelBackupSubscription()
-for response in stub.SubscribeChannelBackups(request, metadata=[('macaroon', macaroon)]):
-    print(response)
